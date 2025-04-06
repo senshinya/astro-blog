151147f732904084d4363cf56c3a283e48ff2b60
shinya <kobayashi_shinya@outlook.com>
2025-04-06 23:44:18 +0800

    feat: add memos page

--- src/utils/page.ts
+++ src/utils/page.ts
@@ -20,6 +20,10 @@ export function isTagPage(path: string) {
   const clean = cleanPath(path)
   return clean.startsWith('tags') || moreLocales.some(lang => clean.startsWith(`${lang}/tags`))
 }
+export function isMemosPage(path: string) {
+  const clean = cleanPath(path)
+  return clean.startsWith('memos') || moreLocales.some(lang => clean.startsWith(`${lang}/memos`))
+}
 export function isAboutPage(path: string) {
   const clean = cleanPath(path)
   return clean.startsWith('about') || moreLocales.some(lang => clean.startsWith(`${lang}/about`))
@@ -34,6 +38,7 @@ export function getPageInfo(path: string) {
     isHome: isHomePage(path),
     isPost: isPostPage(path),
     isTag: isTagPage(path),
+    isMemos: isMemosPage(path),
     isAbout: isAboutPage(path),
     getLocalizedPath: (targetPath: string) => getLocalizedPath(targetPath, currentLang),
   }
