e0621933aa9d4dd9b8933af9411230ff7563494a
shinya <kobayashi_shinya@outlook.com>
2025-06-13 10:20:12 +0800

    adjust search style

--- uno.config.ts
+++ uno.config.ts
@@ -44,6 +44,7 @@ export default defineConfig({
       navbar: ['STIX-Italic', 'EarlySummer-Subset', 'EarlySummer', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
       time: ['Snell-Bold', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
       serif: ['STIX', 'EarlySummer', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+      search: ['STIX-Italic', 'EarlySummer-Subset', 'EarlySummer', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
     },
   },
   rules: [
